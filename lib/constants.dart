f57707352ea111579ff405b2e5b12b821286dd87
Abu Anwar <abuanwar072@gmail.com>
2021-01-22 08:31:30 +0600

    initial

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kSecondaryColor = Color(0xFF8B94BC);
+const kGreenColor = Color(0xFF6AC259);
+const kRedColor = Color(0xFFE92E30);
+const kGrayColor = Color(0xFFC1C1C1);
+const kBlackColor = Color(0xFF101010);
+const kPrimaryGradient = LinearGradient(
+  colors: [Color(0xFF46A0AE), Color(0xFF00FFCB)],
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+);
+
+const double kDefaultPadding = 20.0;
---
+++ lib/models/Questions.dart
@@ -0,0 +1,35 @@
+class Question {
+  final int id, answer;
+  final String question;
+  final List<String> options;
+
+  Question({this.id, this.question, this.answer, this.options});
+}
+
+const List sample_data = [
+  {
+    "id": 1,
+    "question":
+        "Flutter is an open-source UI software development kit created by ______",
+    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "answer_index": 1,
+  },
+  {
+    "id": 2,
+    "question": "When google release Flutter.",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answer_index": 2,
+  },
+  {
+    "id": 3,
+    "question": "A memory location that holds a single letter or number.",
+    "options": ['Double', 'Int', 'Char', 'Word'],
+    "answer_index": 2,
+  },
+  {
+    "id": 4,
+    "question": "What command do you use to output data to the screen?",
+    "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
+    "answer_index": 2,
+  },
+];
